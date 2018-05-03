526c3301aaab4d51d9e6977e82a09c44a1784348
Imma7 <immamugambi@gmail.com>
2018-05-03 05:25:23 +0300

    Creating properties for our services

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -5,6 +5,11 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProfileService {
 
-  constructor() { }
+  private username: string;
+  private clientid = '4193ec64f95f88022fb3';
+  private clientsecret = '5ab8a176a8d93c1c740400f04c70d57bfee1eb82';
+  constructor(private http: Http) {
+
+  }
 
 }
