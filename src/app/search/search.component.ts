259fa0349e23749b5fc7771e539cafb1ec340cfa
Imma7 <immamugambi@gmail.com>
2018-05-03 10:18:35 +0300

    Import forms module

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,19 +9,24 @@ import { ProfileService } from '../services/profile.service';
 export class SearchComponent implements OnInit {
   profile: any[];
   repos: any[];
+  username: string;
 
   constructor(private profileService: ProfileService) {
+
+  }
+
+  findProfile() {
+    this.profileService.updateProfile(this.username);
     this.profileService.getProfileInfo().subscribe(profile => {
-      console.log(profile);
-      this.profile = profile;
+        console.log(profile);
+        this.profile = profile;
     });
 
     this.profileService.getProfileRepos().subscribe(repos => {
-      console.log(repos);
-      this.repos = repos;
+        console.log(repos);
+        this.repos = repos;
     });
-
-   }
+  }
 
   ngOnInit() {
   }
