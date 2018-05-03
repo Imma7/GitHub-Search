af8ea00248aa146779933b4074fd813ce046b590
Imma7 <immamugambi@gmail.com>
2018-05-03 05:41:24 +0300

    Imported Http Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
-import { ProfileService } from '.services/profile.service';
+import { ProfileService } from './services/profile.service';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
