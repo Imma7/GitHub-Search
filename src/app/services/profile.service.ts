f25d0f20ad4d513c9e29e458d32a5595ec5adfa4
Imma7 <immamugambi@gmail.com>
2018-05-03 05:33:00 +0300

    Added function getProfileInfo to get all the data from Github API

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -9,7 +9,12 @@ export class ProfileService {
   private clientid = '4193ec64f95f88022fb3';
   private clientsecret = '5ab8a176a8d93c1c740400f04c70d57bfee1eb82';
   constructor(private http: Http) {
-
+    console.log('Service is ready!');
+    this.username = 'Imma7';
   }
 
+  getProfileInfo() {
+    // tslint:disable-next-line:max-line-length
+    return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret);
+  }
 }
