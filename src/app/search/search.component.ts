7ae9df12411c5877fbda55ba3aff11e17ddc3093
Imma7 <immamugambi@gmail.com>
2018-05-03 05:58:35 +0300

    Set profile variable

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,10 +7,12 @@ import { ProfileService } from '../services/profile.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  private profile: any[];
 
   constructor(private profileService: ProfileService) {
     this.profileService.getProfileInfo().subscribe(profile => {
       console.log(profile);
+      this.profile = profile;
     });
    }
 
